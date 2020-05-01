5cae1100c5f4c29cd860465e4958a38b45d2eea4
Guille <thecrow88@gmail.com>
2020-05-01 12:34:54 +0200

    refactor(test): JUnit5 test removed. JUnit4 test is used for instrumentation tests.

--- myencrypt/src/androidTest/java/com/gcuestab/myencrypt/MyEncryptTest.kt
+++ myencrypt/src/androidTest/java/com/gcuestab/myencrypt/MyEncryptTest.kt
@@ -1,68 +1,54 @@
 package com.gcuestab.myencrypt
 
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
 
 class MyEncryptTest {
 
     lateinit var crypt: MyEncrypt
 
-    @BeforeEach
+    @Before
     fun setUp() {
         crypt =
             provideEncrypt(context = InstrumentationRegistry.getInstrumentation().context)
     }
 
-    @DisplayName(value = "Given empty text")
-    @Nested
-    inner class EmptyText {
-        private val text = ""
-
-        @DisplayName(value = "When encrypt text, then result is empty text")
-        @Test
-        fun whenEncrypt_thenEmpty() {
-            val result = crypt.encrypt(text = text)
-            assert(result.isNotEmpty())
-        }
-
-        @DisplayName(value = "When decrypt text, then result is empty")
-        @Test
-        fun whenDecrypt_thenEmpty() {
-            val result = crypt.decrypt(encryptedText = text)
-            assertEquals("", result)
-        }
+    @Test
+    fun givenEmptyText_whenEncrypt_thenResultIsNotEmpty() {
+        val text = ""
+        val result = crypt.encrypt(text = text)
+        assert(result.isNotEmpty())
     }
 
-    @DisplayName(value = "Given a not empty text")
-    @Nested
-    inner class NotEmptyText {
-        private val text = "My name is Test"
+    @Test
+    fun givenEmptyText_whenDecrypt_thenResultIsEmpty() {
+        val text = ""
+        val result = crypt.decrypt(encryptedText = text)
+        assertEquals("", result)
+    }
 
-        @DisplayName(value = "When encrypt text, then result is not empty")
-        @Test
-        fun whenEncrypt_thenNotEmpty() {
-            val result = crypt.encrypt(text = text)
-            assert(result.isNotEmpty())
-        }
+    @Test
+    fun givenANotEmptyText_whenEncrypt_thenResultIsNotEmpty() {
+        val text = "My name is Test"
+        val result = crypt.encrypt(text = text)
+        assert(result.isNotEmpty())
+    }
 
-        @DisplayName(value = "When decrypt text, then result is not empty")
-        @Test
-        fun whenDecrypt_thenNotEmpty() {
-            val result = crypt.decrypt(encryptedText = text)
-            assert(result.isNotEmpty())
-        }
+    @Test
+    fun givenANotEmptyText_whenDecrypt_thenResultIsNotEmpty() {
+        val text = "My name is Test"
+        val result = crypt.decrypt(encryptedText = text)
+        assert(result.isNotEmpty())
+    }
 
-        @DisplayName(value = "When encrypt and decrypt text, then result is the same than the original text")
-        @Test
-        fun whenEncryptAndDecrypt_thenSameText() {
-            val result = crypt.decrypt(
-                encryptedText = crypt.encrypt(text = text)
-            )
-            assertEquals(text, result)
-        }
+    @Test
+    fun givenANotEmptyText_whenEncryptAndDecrypt_thenSameText() {
+        val text = "My name is Test"
+        val result = crypt.decrypt(
+            encryptedText = crypt.encrypt(text = text)
+        )
+        assertEquals(text, result)
     }
 }
